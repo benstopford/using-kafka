447c540279e47eaec1fadc88ac0ce74fc5240140
Ben Stopford <benstopford@gmail.com>
2016-03-22 11:24:55 +0000

    Added simple test of kafka functionality.

---
+++ src/main/java/com/benstopford/kafka/examples/ToDo.java
@@ -0,0 +1,17 @@
+package com.benstopford.kafka.examples;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class ToDo {
+    public static void main(String[] args) {
+        new ToDo();
+    }
+
+    public ToDo() {
+//        KafkaProducer producer = new KafkaProducer<String,String>();
+//        ProducerRecord record = new ProducerRecord<String,String>("my-topic", "my-key", "my-value");
+//        producer.send(record);
+    }
+
+}
